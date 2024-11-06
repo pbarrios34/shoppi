b660955a9947cdde647609e4df4eae58cf08d90e
Paolo Barrios <barrios.paolo1993@gmail.com>
2024-11-05 22:14:40 -0600

    icons

--- src/Components/ProductDetail/index.jsx
+++ src/Components/ProductDetail/index.jsx
@@ -1,3 +1,4 @@
+import { XMarkIcon } from '@heroicons/react/24/solid'
 import './styles.css'
 
 const ProductDetail = () => {
@@ -5,10 +6,11 @@ const ProductDetail = () => {
         <aside className="product-detail flex flex-col fixed bg-white right-0 border border-black rounded-lg">
             <div className='flex justify-between items-center p-6'>
                 <h2 className='font-medium text-xl'>Product Detail</h2>
-                <div>X</div>
+                <XMarkIcon className="size-6 text-blue-500" />
             </div>
         </aside>
     )
 }
+
 
 export { ProductDetail }
